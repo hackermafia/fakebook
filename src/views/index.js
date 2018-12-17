45726d4d399d8586c73decf53c50ad256de186ea
FelipeAcosta02 <jesusfacosta02@gmail.com>
2018-12-17 16:57:56 -0700

    RIGHT BEFORE API

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 export { default as Home } from './Home/home';
 export { default as PRIVATE_Posts } from './Posts/posts'
+export { default as PRIVATE_Account } from './Account/Screens/Account'
 export { default as SignIn } from './Authenticator/Screens/SignIn';
 export { default as SignUp } from './Authenticator/Screens/SignUp';
 export { default as ForgotPassword } from './Authenticator/Screens/ForgotPassword';
